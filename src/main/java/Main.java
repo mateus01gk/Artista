2e8ab80d8256fc841948188eb2970514c6b49047
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-05-14 00:46:52 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+    System.out.println("***************************");
+    System.out.println("*Artista: Marília Mendonça*");
+    System.out.println("***************************");
+    System.out.println(" ");
+    System.out.println("############################################");
+    System.out.println("# Álbum da Marília Mendonça Decretos Reais #");
+    System.out.println("############################################");
+    System.out.println(" ");
+    System.out.println("Lançamento do Álbum Decretos Reais");
+    System.out.println(" 25 de maio de 2023");
+    System.out.println(" ");
+    System.out.println("==============================================================");
+
+    System.out.println("Decretos Reais é um álbum ao vivo da cantora e compositora brasileira Marília Mendonça, lançado em maio de 2023 pela gravadora brasileira Som Livre. É o primeiro álbum póstumo da artista.");
+    System.out.println("============================================================== ");
+    System.out.println(" ");
+    System.out.println("+++++++++++++++++++++++");
+    System.out.println("+ Minutagem e Músicas +");
+    System.out.println("+++++++++++++++++++++++");
+    System.out.println("  ");
+
+    System.out.println("1 - Leão Minutos 2:47");
+    System.out.println(" ");
+    System.out.println("2 - Uma Vida a Mais Minutos 3:45 ");
+    System.out.println(" ");
+    System.out.println("3 - Me Ame Mais Minutos 2:37");    
+    System.out.println(" ");
+    System.out.println("4 - Te Amo Demais Minutos 3:56");
+    System.out.println(" ");
+    System.out.println("5 - O que Falta em Você Minutos 3:12");
+    System.out.println(" ");
+    System.out.println("6 - Morango do Nordeste Minutagem 5:13");
+    System.out.println(" ");
+    System.out.println("7 - Pra falar a Verdade Minutagem 4:13");
+    System.out.println(" ");
+  
+    System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+  }
+}
